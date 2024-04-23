e06051885a604abcad2e46b3ff53b32ed32a82c4
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-23 15:12:11 -0600

    fondo con gradiente

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -11,17 +11,55 @@ class PlayerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
+      // Fondo y Contenido Principal
+      body: Stack(
         children: [
-          // Barra de aplicaciones
-          CustomAppBar(),
-          // Portada y Duración
-          CoverAndDuration(),
-          // Título y botón de reproducir
-          TitleAndPlayButton(),
-          // Lírica o letra de la canción
-          Lyric(),
+          // Fondo gradiente
+          _Background(),
+          // Contenido principal
+          Column(
+            children: [
+              // Barra de aplicaciones
+              CustomAppBar(),
+              // Portada y Duración
+              CoverAndDuration(),
+              // Título y botón de reproducir
+              TitleAndPlayButton(),
+              // Lírica o letra de la canción
+              Lyric(),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+// Widget referente al fondo degradado de la pantalla
+class _Background extends StatelessWidget {
+  const _Background({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtener el tamaño real de la pantalla
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      // El alto de este widget será del 80% de la pantalla del dispositivo
+      height: screenSize.height * 0.8,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff33333e),
+            Color(0xff201e28),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.center,
+        ),
       ),
     );
   }
